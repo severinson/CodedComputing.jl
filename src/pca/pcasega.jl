b95e32aff566dd429b3ac1fb7262544f576075b8
Albin Severinson <albin@severinson.org>
2020-11-26 16:44:54 +0100

    Variance-reduced PCA

---
+++ src/pca/pcasega.jl
@@ -0,0 +1,64 @@
+function problem_size(filename::String, dataset::String)
+    HDF5.ishdf5(filename) || throw(ArgumentError("$filename isn't an HDF5 file"))
+    h5open(filename, "r") do fid
+        flag, msg = isvalidh5csc(fid, dataset)
+        if !flag
+            throw(ArgumentError(msg))
+        end
+        g = fid[dataset]
+        return g["m"][], g["n"][]
+    end
+end
+
+function read_localdata(filename::String, dataset::String, i::Integer, npartitions::Integer)
+    HDF5.ishdf5(filename) || throw(ArgumentError("$filename isn't an HDF5 file"))
+    X = h5readcsc(filename, dataset)
+    m = size(X, 1)
+    il = round(Int, (i - 1)/npartitions*m + 1)
+    iu = round(Int, i/npartitions*m)
+    Xw = X[il:iu, :]
+    Xw
+end
+
+function worker_task!(V, Xw, state=nothing)
+    if isnothing(state)
+        W = Matrix{eltype(V)}(undef, size(Xw, 1), size(V, 2))
+    else
+        W = state
+    end
+    mul!(W, Xw, V)
+    mul!(V, Xw', W)
+    W
+end
+
+function update_gradient!(∇, Vs, epoch::Integer, repochs::Vector{<:Integer}, state=nothing)
+    length(Vs) == length(repochs) || throw(DimensionMismatch("Vs has dimension $(length(Vs)), but repochs has dimension $(length(repochs))"))
+    nworkers = length(Vs)
+    if isnothing(state)
+        ∇s = [similar(∇) for _ in 1:nworkers]
+        for ∇i in ∇s
+            ∇i .= 0
+        end
+        uepochs = similar(repochs)
+        uepochs .= -1
+    else
+        ∇s, uepochs = state
+    end    
+    length(uepochs) == length(repochs) || throw(DimensionMismatch("uepochs has dimension $(length(uepochs)), but repochs has dimension $(length(repochs))"))
+    ∇ .= 0
+    for i in 1:nworkers
+        if repochs[i] > uepochs[i]
+            ∇s[i] .= Vs[i]
+        end
+        ∇ .+= ∇s[i]
+    end
+    ∇s, uepochs
+end
+
+function update_iterate!(V, ∇, state=nothing)
+    V .= ∇
+    orthogonal!(V)
+    state
+end
+
+include("common.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -109,6 +109,50 @@ end
     end
 end
 
+@testset "pcasega.jl" begin
+
+    # setup
+    Random.seed!(123)
+    kernel = "../src/pca/pcasega.jl"
+    nworkers = 2
+    niterations = 200
+    inputdataset = "X"
+    outputdataset = "V"
+    n, m = 20, 10
+    k = m
+    p = 0.9 # matrix density
+
+    # generate input dataset
+    X = sprand(n, m, p)
+    inputfile = tempname()
+    h5writecsc(inputfile, inputdataset, X)
+
+    # correct solution (computed via LinearAlgebra.svd)
+    V_correct = pca(Matrix(X), k)
+    V = similar(V_correct)
+
+    ### exact
+    outputfile = tempname()
+    mpiexec(cmd -> run(`$cmd -n $(nworkers+1) julia --project $kernel $inputfile $outputfile --niterations $niterations --ncomponents $k`))
+
+    # test that the output was generated correctly
+    @test HDF5.ishdf5(outputfile)
+    h5open(outputfile, "r") do fid
+        @test outputdataset in keys(fid)
+        @test size(fid[outputdataset]) == (m, k)
+        V .= fid[outputdataset][:, :]
+        @test length(fid["benchmark/t_compute"]) == niterations
+    end
+
+    # compare the computed principal components with those obtained from the built-in svd
+    for i in 1:k
+        @test isapprox(
+            CodedComputing.minangle(view(V, :, i), view(V_correct, :, i)),
+            0, atol=1e-2
+        )
+    end
+end
+
 @testset "HDF5Sparse.jl" begin
     Random.seed!(123)
     m, n, p = 10, 5, 0.1
