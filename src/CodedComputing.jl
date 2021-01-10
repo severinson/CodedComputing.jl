a1314540e4a1a64f125f760ef5bbae0649c7321b
Albin Severinson <albin@severinson.org>
2021-01-10 17:58:50 +0100

    Added module for loading the MNIST dataset

--- src/CodedComputing.jl
+++ src/CodedComputing.jl
@@ -3,6 +3,7 @@ module CodedComputing
 using LinearAlgebra, SparseArrays, HDF5
 
 include("Linalg.jl")
+include("Datasets.jl")
 include("HDF5Sparse.jl")
 
 end
---
+++ src/Datasets.jl
@@ -0,0 +1,28 @@
+using MLDatasets
+
+"""
+    load_dataset(n, d, f; filename::AbstractString)
+
+Load a dataset by calling the function f, which is assumed to return an array, reshape that array
+into a matrix of size `n` by `d` and return it. If filename is a string, the array is cached on 
+disk (in a file with the given filename) to speed up subsequent calls.
+"""
+function load_dataset(n, d, f; filename::Union{Nothing,<:AbstractString})
+    try # try to load from cache
+        return Matrix(reshape(
+            reinterpret(
+                FixedPointNumbers.Normed{UInt8, 8},
+                read(filename),
+            ), n, d))
+    catch SystemError # load from MLDatasets (about 10x slower)
+        T = f()
+        X = matrix_from_tensor(T)
+        if !isnothing(filename)
+            write(filename, X) # cache on disk to speed up subsequent calls
+        end
+        return X
+    end
+end
+
+load_mnist_training() = load_dataset(60000, 784, MLDatasets.MNIST.traintensor, filename="MNIST_training.bin")
+load_mnist_testing() = load_dataset(10000, 784, MLDatasets.MNIST.testtensor, filename="MNIST_testing.bin")
