10951043f20be024d1f99925801db7fb546f6370
Albin Severinson <albin@severinson.org>
2021-01-14 19:53:18 +0100

    Memoize results from parsed files

--- src/analysis/pca.jl
+++ src/analysis/pca.jl
@@ -1,44 +1,60 @@
 using HDF5, DataFrames, CSV, Glob
 using CodedComputing
 
-function parse_output_file(jobid::Integer, fid, inputmatrix)
-    rv = DataFrame()    
+function parse_output_file(filename::AbstractString, inputmatrix)    
+    println("parsing $filename")    
 
-    row = Dict{String,Any}()
-    row["nrows"] = size(inputmatrix, 1)
-    row["ncolumns"] = size(inputmatrix, 2)
-    niterations = fid["parameters/niterations"][]
-    nworkers = fid["parameters/nworkers"][]            
+    # return a memoized result if one exists
+    df_filename = filename * ".csv"
+    if isfile(df_filename)
+        return DataFrame(CSV.File(df_filename))
+    end
 
-    # store a unique ID for each file read
-    row["jobid"] = jobid
+    # skip non-existing/non-hdf5 files
+    rv = DataFrame()
+    if !HDF5.ishdf5(filename)
+        println("skipping (not a HDF5 file): $filename")
+        return rv
+    end    
 
-    # store all parameters the job was run with
-    if "parameters" in keys(fid) && typeof(fid["parameters"]) <: HDF5.Group
-        g = fid["parameters"]
-        for key in keys(g)
-            value = g[key][]
-            row[key] = value
+    h5open(filename) do fid        
+        row = Dict{String,Any}()
+        row["nrows"] = size(inputmatrix, 1)
+        row["ncolumns"] = size(inputmatrix, 2)
+        niterations = fid["parameters/niterations"][]
+        nworkers = fid["parameters/nworkers"][]
+
+        # store all parameters the job was run with
+        if "parameters" in keys(fid) && typeof(fid["parameters"]) <: HDF5.Group
+            g = fid["parameters"]
+            for key in keys(g)
+                value = g[key][]
+                row[key] = value
+            end
         end
-    end
 
-    # compute mse
-    mses = Vector{Union{Missing,Float64}}(missing, niterations)
-    if "iterates" in keys(fid)
-        iterates = [fid["iterates"][:, :, i] for i in 1:niterations]        
-        Threads.@threads for i in 1:niterations
-            mses[i] = explained_variance(inputmatrix, iterates[i])
+        # compute mse
+        mses = Vector{Union{Missing,Float64}}(missing, niterations)
+        if "iterates" in keys(fid)
+            iterates = [fid["iterates"][:, :, i] for i in 1:niterations]        
+            Threads.@threads for i in 1:niterations
+                mses[i] = explained_variance(inputmatrix, iterates[i])
+            end
         end
-    end
 
-    # add benchmark data
-    for i in 1:niterations
-        row["iteration"] = i
-        row["mse"] = mses[i]
-        row["t_compute"] = fid["benchmark/t_compute"][i]
-        row["t_update"] = fid["benchmark/t_update"][i]
-        push!(rv, row, cols=:union)
-    end
+        # add benchmark data
+        for i in 1:niterations
+            row["iteration"] = i
+            row["mse"] = mses[i]
+            row["t_compute"] = fid["benchmark/t_compute"][i]
+            row["t_update"] = fid["benchmark/t_update"][i]
+            push!(rv, row, cols=:union)
+        end
+    end    
+
+    # memoize the resulting df
+    CSV.write(df_filename, rv)
+
     rv
 end
 
@@ -64,15 +80,8 @@ function aggregate_benchmark_data(;dir="/shared/201124/3/", inputfile="/shared/2
     filenames = glob("$(prefix)*.h5", dir)    
     dfs = Vector{DataFrame}(undef, length(filenames))
     for (i, filename) in collect(enumerate(filenames))
-
-        println("parsing $filename")
-        if !HDF5.ishdf5(filename)
-            println("skipping (not a HDF5 file): $filename")
-            return rv
-        end
-        h5open(filename) do fid        
-            dfs[i] = parse_output_file(i, fid, X)
-        end
+        dfs[i] = parse_output_file(filename, X)
+        dfs[i][:jobid] = i # store a unique ID for each file read
     end
 
     # concatenate, write to disk, and return
