bd93005b9ad16e3091b865626599ded314321a2f
Albin Severinson <albin@severinson.org>
2021-03-04 18:16:50 +0100

    Added code for writing the whole genome to .h5 files

--- src/Datasets.jl
+++ src/Datasets.jl
@@ -23,8 +23,30 @@ end
 load_mnist_training() = load_dataset(60000, 784, MLDatasets.MNIST.traintensor, filename="MNIST_training.bin")
 load_mnist_testing() = load_dataset(10000, 784, MLDatasets.MNIST.testtensor, filename="MNIST_testing.bin")
 
-function load_genome_data(chr=20; directory="./1000genomes")
+"""
+    load_genome_data(chr; directory="./1000genomes/parsed")
+
+Read parsed plain-text genome data and return it as a boolean `SparseMatrixCSC`, where the 
+`i, j`-th entry indicates if there is a mutation in the `j`-th position of the genom of the `i`-th 
+subject (i.e., rows correspond to subjects and columns to genome positions).
+"""
+function load_genome_data(chr; directory="./1000genomes/parsed")
     filename = joinpath(directory, "chr$(chr).txt")
     df = DataFrame(CSV.File(filename))
     sparse(df[:, 2], df[:, 1], true)
+end
+
+"""
+
+Read parsed plain-text genome data for all chromosomes and write it to a single matrix in order.
+"""
+function genome_to_hdf5(filename="./1000genomes/parsed/1000genomes.h5")
+    println("Parsing chromosome 1")
+    X = load_genome_data(1)
+    h5writecsc(filename, "X", X)
+    for chr in 2:22
+        GC.gc() # force garbage collection to avoid running out of memory
+        println("Parsing chromosome $chr")
+        h5appendcsc(filename, "X", load_genome_data(chr))
+    end
 end
