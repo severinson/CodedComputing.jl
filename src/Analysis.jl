294effdb3821dafc22cfa5d63c4a6795eac98762
Albin Severinson <albin@severinson.org>
2021-04-06 13:31:30 +0200

    Refactoring, removed unused code

--- src/Analysis.jl
+++ src/Analysis.jl
@@ -48,7 +48,80 @@ function write_table(xs::AbstractVector, ys::AbstractVector, filename::AbstractS
     return
 end
 
-includet("analysis/preprocessing.jl")
+"""
+
+Return a df composed of the order statistic samples for each worker, iteration, and job.
+"""
+function tall_from_wide(df; extend=false)
+    df = df[.!ismissing.(df["latency_worker_1"]), :]    
+    if size(df, 1) == 0
+        return DataFrame()
+    end    
+
+    # determine the number of workers based on column names
+    # (since not all rows may be present)
+    cols = [name for name in names(df) if occursin("repoch", name)]
+    indices = parse.(Int, last.(split.(cols, "_")))
+    nworkers = maximum(indices)
+    
+    ### stack by worker latency
+    df1 = select(df, Not(["repoch_worker_$i" for i in 1:nworkers]))
+    if "compute_latency_worker_1" in names(df)
+        select!(df1, Not(["compute_latency_worker_$i" for i in 1:nworkers]))    
+    end    
+    df1 = stack(df1, ["latency_worker_$i" for i in 1:nworkers], value_name=:worker_latency)
+    select!(df1, Not(:variable), :variable => ((x) -> parse.(Int, last.(split.(x, "_")))) => :worker_index)
+    dropmissing!(df1, :worker_latency)
+    
+    # stack by worker receive epoch
+    df2 = stack(df, ["repoch_worker_$i" for i in 1:nworkers], [:jobid, :iteration], value_name=:repoch)
+    select!(df2, Not(:variable), :variable => ((x) -> parse.(Int, last.(split.(x, "_")))) => :worker_index)    
+    dropmissing!(df2, :repoch)
+    df2[:isstraggler] = df2.repoch .< df2.iteration
+    joined = innerjoin(df1, df2, on=[:jobid, :iteration, :worker_index]) 
+
+    # stack by worker compute latency
+    if "compute_latency_worker_1" in names(df)
+        df3 = stack(df, ["compute_latency_worker_$i" for i in 1:nworkers], [:jobid, :iteration], value_name=:worker_compute_latency)
+        select!(df3, Not(:variable), :variable => ((x) -> parse.(Int, last.(split.(x, "_")))) => :worker_index)            
+        dropmissing!(df3, :worker_compute_latency)
+        joined = innerjoin(joined, df3, on=[:jobid, :iteration, :worker_index])
+    end
+
+    # the latency of stragglers is infinite
+    joined[joined.isstraggler, :worker_latency] .= Inf
+
+    # compute the order of all workers
+    sort!(joined, [:jobid, :iteration, :worker_latency])
+    joined[:order] = by(joined, [:jobid, :iteration], :nworkers => ((x) -> collect(1:maximum(x))) => :order).order
+    if "compute_latency_worker_1" in names(df)
+        sort!(joined, [:jobid, :iteration, :worker_compute_latency])
+        joined[:compute_order] = by(joined, [:jobid, :iteration], :nworkers => ((x) -> collect(1:maximum(x))) => :order).order        
+    end
+
+    if extend
+        dfi = joined[joined.nwait .== joined.nworkers, :]
+        dfi.mse = missing
+        for i in 1:maximum(df.nworkers)
+            dfj = dfi
+            dfj = dfj[dfj.nworkers .> i, :]        
+            dfj = dfj[dfj.worker_index .<= i, :]
+            if size(dfj, 1) == 0
+                continue
+            end
+            dfj.nworkers .= i
+            dfj.nwait .= i
+            sort!(dfj, [:jobid, :iteration, :worker_latency])
+            dfj.order .= by(dfj, [:jobid, :iteration], :nworkers => ((x) -> collect(1:maximum(x))) => :order).order
+            joined = vcat(joined, dfj)
+        end
+    end
+    # add a flag indicating if the worker is experiencing a latency burst
+    # joined.burst = burst_state_from_orderstats_df(joined)
+
+    return joined
+end
+
 includet("analysis/convergence.jl")
 includet("analysis/correlation.jl")
 includet("analysis/latency.jl")
