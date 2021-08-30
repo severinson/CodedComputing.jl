3dbf5f595c47b91ca11621daf98ce1fd71bc1bc4
Albin Severinson <albin@severinson.org>
2021-08-30 15:07:03 +0200

    Pass work distribution parameters through the profiler

--- src/profiling.jl
+++ src/profiling.jl
@@ -1,9 +1,30 @@
 # Code used for latency profiling
 using StatsBase, Statistics, OnlineStats, Dates
 
+struct ProfilerInput
+    worker::Int
+    θ::Float64
+    q::Float64
+    timestamp::Time    
+    comp_delay::Float64
+    comm_delay::Float64    
+end
+
+struct ProfilerOutput
+    worker::Int         # worker index
+    θ::Float64          # fraction of the dataset stored by this worker, averaged over all input samples that make up this output
+    q::Float64          # fraction of local data processed per iteration, averaged over all input samples that make up this output
+    comp_mc::Float64    # = (mean comp. delay) / (θ*q)
+    comp_vc::Float64    # = (var of comp. delay) / (θ*q)
+    comm_mc::Float64    # = mean comm. delay
+    comm_vc::Float64    # = var of comm. delay
+end
+
+Base.isless(p::Pair{Time, CodedComputing.ProfilerInput}, q::Pair{Time, CodedComputing.ProfilerInput}) = isless(first(p), first(q))
+
 function setup_profiler_channels(;chin_size=Inf, chout_size=Inf)
-    chin = Channel{@NamedTuple{worker::Int,timestamp::Time,comp::Float64,comm::Float64}}(chin_size)
-    chout = Channel{@NamedTuple{worker::Int,comp_mean::Float64,comp_var::Float64,comm_mean::Float64,comm_var::Float64}}(chout_size)
+    chin = Channel{ProfilerInput}(chin_size)
+    chout = Channel{ProfilerOutput}(chout_size)
     chin, chout
 end
 
@@ -12,41 +33,61 @@ end
 Latency profiling sub-system. Receives latency observations on `chin`, computes the mean and 
 variance over a moving time window of length `windowsize`, and sends the results on `chout`.
 """
-function latency_profiler(
-    chin::Channel{@NamedTuple{worker::Int,timestamp::Time,comp::Float64,comm::Float64}}, 
-    chout::Channel{@NamedTuple{worker::Int,comp_mean::Float64,comp_var::Float64,comm_mean::Float64,comm_var::Float64}};
-    nworkers::Integer, qlower::Real=0.1, qupper::Real=0.9, buffersize::Integer=1000, windowsize::Dates.AbstractTime=Second(60))
-    @info "latency_profiler task started"    
+function latency_profiler(chin::Channel{ProfilerInput}, chout::Channel{ProfilerOutput}; nworkers::Integer, qlower::Real=0.1, qupper::Real=0.9, buffersize::Integer=1000, windowsize::Dates.AbstractTime=Second(60))
+    0 < nworkers || throw(ArgumentError("nworkers is $nworkers"))
+    0 <= qlower <= qupper <= 1.0 || throw(ArgumentError("qlower is $qlower and qupper is $qupper"))
+    @info "latency_profiler task started"
     
     # maintain a window of latency samples for each worker
-    ws = [MovingTimeWindow(windowsize, valtype=@NamedTuple{comp::Float64, comm::Float64}, timetype=Time) for _ in 1:nworkers]
+    ws = [MovingTimeWindow(windowsize, valtype=ProfilerInput, timetype=Time) for _ in 1:nworkers]
 
     # utility function for updating the correct window with a new latency measurement
-    function fitwindow(v)
-        0 < v.worker <= nworkers || @error "Expected v.worker to be in [1, $nworkers], but it is $(v.worker)"
-        if v.comp < 0 || v.comm < 0
-            @error "latency_profiler received negative latency sample" v
+    function process_sample(v)
+        0 < v.worker <= nworkers || throw(ArgumentError("v.worker is $(v.worker), but nworkers is $nworkers"))
+        0 <= v.θ <= 1 || throw(ArgumentError("θ is $θ"))
+        0 <= v.q <= 1 || throw(ArgumentError("q is $q"))
+        0 <= v.comp_delay || throw(ArgumentError("comp_delay is $comp_delay"))
+        0 <= v.comm_delay || throw(ArgumentError("comm_delay is $comm_delay"))
+        if isnan(v.comp_delay) || isnan(v.comm_delay)
             return
         end
-        isnan(v.comm) || isnan(v.comp) || fit!(ws[v.worker], (v.timestamp, @NamedTuple{comp::Float64,comm::Float64}((v.comp, v.comm))))
+        fit!(ws[v.worker], (v.timestamp, v))
         return
     end
 
-    # helper functions for computing the mean and variance over the samples in a window
+    # compute the mean over all values in the window
+    # function window_mean(w::MovingTimeWindow, key::Symbol)¨
+    function window_mean(w, key)
+        rv = 0.0
+        n = 0
+        for (_, t) in value(w)
+            rv += getfield(t, key)
+            n += 1
+        end
+        rv / n
+    end
+
+    # compute the mean and variance over all values in the window between the qlower and qupper quantiles
     buffer = zeros(buffersize)
-    function processwindow(w::MovingTimeWindow, key::Symbol)
-        key == :comp || key == :comm || throw(ArgumentError("key must be either :comp or :comm, but is $key"))        
+    function window_mean_var(w::MovingTimeWindow, key::Symbol)
 
         # populate the buffer
         i = 0
         n = 0
         for (_, t) in value(w)
-            v = t[key]
-            if !isnan(v)
-                buffer[i+1] = v
-                i = mod(i + 1, buffersize)
-                n += 1
+            v = getfield(t, key)
+
+            # compute delay should be normalized
+            if key == :comp_delay
+                v /= (getfield(t, :θ) * getfield(t, :q))
+            end            
+
+            if isnan(v)
+                continue
             end
+            buffer[i+1] = v
+            i = mod(i + 1, buffersize)
+            n += 1
         end
         n = min(n, buffersize)
 
@@ -66,8 +107,19 @@ function latency_profiler(
             return NaN, NaN
         end
         m = mean(vs)
-        v = var(vs, mean=m, corrected=true)        
+        v = var(vs, mean=m, corrected=true)
         m, v
+    end
+
+    # process all samples in the window for the i-th worker
+    function process_worker(i::Integer)
+        0 < i <= nworkers || throw(ArgumentError("i is $i"))
+        w = ws[i]
+        θ = window_mean(w, :θ)
+        q = window_mean(w, :q)
+        comp_mc, comp_vc = window_mean_var(w, :comp_delay)
+        comm_mc, comm_vc = window_mean_var(w, :comm_delay)
+        ProfilerOutput(i, θ, q, comp_mc, comp_vc, comm_mc, comm_vc)
     end
 
     # process incoming latency samples
@@ -76,7 +128,7 @@ function latency_profiler(
         # consume all values currently in the channel
         try
             vin = take!(chin)
-            fitwindow(vin)
+            process_sample(vin)
         catch e
             if e isa InvalidStateException
                 @info "error taking value from input channel" e                
@@ -88,7 +140,7 @@ function latency_profiler(
         while isready(chin)
             try
                 vin = take!(chin)
-                fitwindow(vin)
+                process_sample(vin)
             catch e
                 if e isa InvalidStateException
                     @info "error taking value from input channel" e
@@ -99,17 +151,12 @@ function latency_profiler(
             end
         end        
 
+        # compute updated statistics for all workers
         for i in 1:nworkers
-
-            # for each worker, compute the mean and variance of the values in the window            
-            comp_mean, comp_var = processwindow(ws[i], :comp)
-            comm_mean, comm_var = processwindow(ws[i], :comm)
-            if isnan(comp_mean) || isnan(comp_var) || isnan(comm_mean) || isnan(comm_var)
+            vout = process_worker(i)
+            if isnan(vout.θ) || isnan(vout.q) || isnan(vout.comp_mc) || isnan(vout.comp_vc) || isnan(vout.comm_mc) || isnan(vout.comm_vc)
                 continue
             end
-
-            # push the computed statistics into the output channel
-            vout = @NamedTuple{worker::Int,comp_mean::Float64,comp_var::Float64,comm_mean::Float64,comm_var::Float64}((i,comp_mean,comp_var,comm_mean,comm_var))            
             try
                 push!(chout, vout)
             catch e
--- test/profiling_test.jl
+++ test/profiling_test.jl
@@ -5,6 +5,8 @@ nworkers = 2
 windowsize = Second(10)
 qlower, qupper = 0.0, 1.0
 chin, chout = CodedComputing.setup_profiler_channels()
+θs = [0.3, 0.7]
+qs = 1 ./ [2, 3]
 
 function f(vs)
     l, u = quantile(vs, qlower), quantile(vs, qupper)
@@ -13,37 +15,37 @@ end
 
 # put example values into the input channel
 ## worker 1
-timestamps1 = Time(0) .+ [Second(i) for i in 1:11]
-comps1 = rand(length(timestamps1))
-comms1 = rand(length(timestamps1))
-for i in 1:length(timestamps1)
-    v = @NamedTuple{worker::Int,timestamp::Time,comp::Float64,comm::Float64}((1, timestamps1[i], comps1[i], comms1[i]))
+timestamps = Time(0) .+ [Second(i) for i in 1:11]
+comps = rand(length(timestamps))
+comms = rand(length(timestamps))
+for i in 1:length(timestamps)
+    v = CodedComputing.ProfilerInput(1, θs[1], qs[1], timestamps[i], comps[i], comms[i])
     push!(chin, v)
 end
-comps1 = comps1[end-1:end]
-comms1 = comms1[end-1:end]
-comp_mean, comp_var = mean(comps1), var(comps1, corrected=true)
-comm_mean, comm_var = mean(comms1), var(comms1, corrected=true)
-correct1 = @NamedTuple{worker::Int,comp_mean::Float64,comp_var::Float64,comm_mean::Float64,comm_var::Float64}((1,comp_mean,comp_var,comm_mean,comm_var))
+comps ./= θs[1] * qs[1]
+comps = comps[end-1:end]
+comms = comms[end-1:end]
+comp_mean, comp_var = mean(comps), var(comps, corrected=true)
+comm_mean, comm_var = mean(comms), var(comms, corrected=true)
+correct1 = CodedComputing.ProfilerOutput(1, θs[1], qs[1], comp_mean, comp_var, comm_mean, comm_var)
 
 ## worker 2
-timestamps2 = Time(0) .+ [Second(i) for i in 1:20]
-comps2 = rand(length(timestamps2))
-comms2 = rand(length(timestamps2))
-for i in 1:length(timestamps2)
-    v = @NamedTuple{worker::Int,timestamp::Time,comp::Float64,comm::Float64}((2, timestamps2[i], comps2[i], comms2[i]))
+timestamps = Time(0) .+ [Second(i) for i in 1:20]
+comps = rand(length(timestamps))
+comms = rand(length(timestamps))
+for i in 1:length(timestamps)
+    v = CodedComputing.ProfilerInput(2, θs[2], qs[2], timestamps[i], comps[i], comms[i])    
     push!(chin, v)
 end
-comps2 = f(comps2[end-10:end])
-comms2 = f(comms2[end-10:end])
-comp_mean, comp_var = mean(comps2), var(comps2, corrected=true)
-comm_mean, comm_var = mean(comms2), var(comms2, corrected=true)
-correct2 = @NamedTuple{worker::Int,comp_mean::Float64,comp_var::Float64,comm_mean::Float64,comm_var::Float64}((2,comp_mean,comp_var,comm_mean,comm_var))
+comps ./= θs[2] * qs[2]
+comps = f(comps[end-10:end])
+comms = f(comms[end-10:end])
+comp_mean, comp_var = mean(comps), var(comps, corrected=true)
+comm_mean, comm_var = mean(comms), var(comms, corrected=true)
+correct2 = CodedComputing.ProfilerOutput(2, θs[2], qs[2], comp_mean, comp_var, comm_mean, comm_var)
 
 # start the task and test that it consumes the input values
 task = Threads.@spawn CodedComputing.latency_profiler(chin, chout; nworkers, qlower, qupper, windowsize)
-
-# sleep(10)
 
 # wait for up to 10 seconds for the input to be consumed
 t0 = time_ns()
@@ -58,26 +60,27 @@ while (time_ns() - t0)/1e9 < 10 && !isready(chout)
     sleep(0.1)
 end
 
+# stop the profiler
+close(chin)
+wait(task)
+
 # there should be 2 values in the output channel
 ## test the first output value
 @test isready(chout)
 v = take!(chout)
 @test v.worker == 1 || v.worker == 2
-if v.worker == 1
-    @test all(isapprox.(values(v), values(correct1)))
-elseif v.worker == 2
-    @test all(isapprox.(values(v), values(correct2)))
+correct = v.worker == 1 ? correct1 : correct2
+for name in fieldnames(CodedComputing.ProfilerOutput)
+    @test getfield(correct, name) ≈ getfield(v, name)
 end
+# @test all(isapprox.(values(v), values(correct1)))
 
 ## test the second output value
 @test isready(chout)
 v = take!(chout)
-@test v.worker == 1 || v.worker == 2
-if v.worker == 1
-    @test all(isapprox.(values(v), values(correct1)))
-elseif v.worker == 2
-    @test all(isapprox.(values(v), values(correct2)))
+correct = v.worker == 1 ? correct1 : correct2
+for name in fieldnames(CodedComputing.ProfilerOutput)
+    @test getfield(correct, name) ≈ getfield(v, name)
 end
 
-close(chin)
 close(chout)
