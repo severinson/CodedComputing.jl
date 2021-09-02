931f91904d72ea1ba42e3a7817be4e5adbeb94bf
Albin Severinson <albin@severinson.org>
2021-09-02 14:17:15 +0200

    Fixed counting number of initialized samples

--- src/treegradient.jl
+++ src/treegradient.jl
@@ -1,16 +1,22 @@
-export TreeGradient, nsamples
+export TreeGradient, TreeGradientNode
+
+mutable struct TreeGradientNode{T}
+   ∇::T
+   isinitialized::Bool
+end
 
 mutable struct TreeGradient{T}
     ∇::T        # overall gradient (sum of all gradients in t)
     n::Int      # total number of samples
     ninit::Int  # number of initialized samples
-    t::DataStructures.BalancedTree23{Int,T,Base.Order.ForwardOrdering}
+    t::DataStructures.BalancedTree23{Int,TreeGradientNode{T},Base.Order.ForwardOrdering}
 end
 
-function TreeGradient(∇::T, n::Integer) where T
-    t = DataStructures.BalancedTree23{Int,T,Base.Order.ForwardOrdering}(Base.Order.ForwardOrdering())
-    insert!(t, 1, ∇, false)
-    TreeGradient{T}(copy(∇), n, n, t)
+function TreeGradient(∇::T, n::Integer; isempty::Bool=false) where T
+    t = DataStructures.BalancedTree23{Int,TreeGradientNode{T},Base.Order.ForwardOrdering}(Base.Order.ForwardOrdering())
+    insert!(t, 1, TreeGradientNode(∇, !isempty), false)
+    ninit = isempty ? 0 : n
+    TreeGradient(copy(∇), n, ninit, t)
 end
 
 Base.size(tg::TreeGradient) = size(tg.∇)
@@ -39,13 +45,23 @@ initialized_fraction(tg::TreeGradient) = tg.ninit / tg.n
 
 """
 
+Return the number of samples that make up the sub-gradient at `tg.t.data[i]`.
+"""
+function subgradient_nsamples(tg::TreeGradient, i::Integer)
+    first_sample = tg.t.data[i].k
+    next = DataStructures.nextloc0(tg.t, i)
+    final_sample = next == 2 ? tg.n : tg.t.data[next].k-1
+    final_sample - first_sample + 1
+end
+
+"""
+
 Insert the gradient with respect to samples `i` through `j`, with value `∇i`, into `t`. If 
 `isempty` is `false`, the inserted gradient is counted towards the number of initialized samples.
 """
-function Base.insert!(tg::TreeGradient{T}, i::Integer, j::Integer, ∇i::T, isempty::Bool=false) where T
+function Base.insert!(tg::TreeGradient{T}, i::Integer, j::Integer, ∇i::T; isempty::Bool=false) where T
     0 < i <= j <= tg.n || throw(ArgumentError("invalid (i, j): $((i, j))"))
     size(∇i) == size(tg) || throw(DimensionMismatch("∇i has dimensions $(size(∇i)), but existing data has dimensions $(size(tg))"))
-
     prev, _ = DataStructures.findkey(tg.t, i)
     prev_key = tg.t.data[prev].k
     prev_val = tg.t.data[prev].d
@@ -53,25 +69,33 @@ function Base.insert!(tg::TreeGradient{T}, i::Integer, j::Integer, ∇i::T, isem
     next = DataStructures.nextloc0(tg.t, k)
     next_key = next == 2 ? tg.n+1 : tg.t.data[next].k
 
-    # remove the previous value from the total, and add the new value to be inserted
-    tg.∇ .-= prev_val
-    if !isempty
-        tg.∇ .+= ∇i
+    # take out the previous sub-gradient
+    if prev_val.isinitialized
+        tg.∇ .-= prev_val.∇
+        tg.ninit -= subgradient_nsamples(tg, prev)
     end
 
-    # update the count of initialized samples
-    tg.ninit = max(tg.ninit - (next_key - prev_key), 0)
-    tg.ninit = min(tg.ninit+j-i+1, tg.n)
+    # add the new sub-gradient
+    if !isempty
+        tg.∇ .+= ∇i
+        tg.ninit += j - i + 1
+    end    
+
 
     # delete any intermediate nodes
     k = DataStructures.nextloc0(tg.t, prev)
     key = k == 2 ? tg.n+1 : tg.t.data[k].k
     ∇r1, ∇r2 = ∇i, ∇i # store pointers to data allocated to deleted nodes, so that we can recycle the memory
     while key < next_key
+        node = tg.t.data[k]
         ∇r1 = ∇r2
-        ∇r2 = tg.t.data[k].d
-        tg.∇ .-= tg.t.data[k].d
-        delete!(tg.t, k)        
+        ∇r2 = node.d.∇
+        if node.d.isinitialized
+            tg.∇ .-= node.d.∇
+            tg.ninit -= subgradient_nsamples(tg, k)
+        end
+        delete!(tg.t, k)
+
         k = DataStructures.nextloc0(tg.t, prev)
         key = k == 2 ? tg.n+1 : tg.t.data[k].k
     end
@@ -79,17 +103,19 @@ function Base.insert!(tg::TreeGradient{T}, i::Integer, j::Integer, ∇i::T, isem
     # either zero out the node to the left and insert a new node, or, if i matches the start of an
     # existing node, update that node in-place (to avoid mutating the tree unnecessarily)
     if prev_key < i
-        prev_val .= 0
-
+        prev_val.∇ .= 0            
+        prev_val.isinitialized = false
+        
         # if a node was deleted, we can re-use the memory allocated for it
         if !(∇r2 === ∇i)
             ∇r2 .= ∇i
         else
             ∇r2 = copy(∇i)
         end
-        insert!(tg.t, i, ∇r2, false)
+        insert!(tg.t, i, TreeGradientNode(∇r2, !isempty), false)
     else
-        prev_val .= ∇i
+        prev_val.∇ .= ∇i
+        prev_val.isinitialized = !isempty
     end    
     if j < next_key - 1
 
@@ -99,7 +125,7 @@ function Base.insert!(tg::TreeGradient{T}, i::Integer, j::Integer, ∇i::T, isem
         else
             ∇r1 = zero(∇i)
         end
-        insert!(tg.t, j+1, ∇r1, false)
+        insert!(tg.t, j+1, TreeGradientNode(∇r1, false), false)
     end
     DataStructures.findkeyless(tg.t, i)
 end
--- test/treegradient_test.jl
+++ test/treegradient_test.jl
@@ -1,67 +1,143 @@
 function tg_sum(tg::TreeGradient)
     rv = zero(tg.∇)
-    for (_, v) in tg
-        rv .+= v
+    for (_, node) in tg
+        rv .+= node.∇
     end
     rv
+end
+
+function tg_cmp(a, b)
+    key_a, node_a = a
+    key_b, node_b = b
+    @test key_a == key_b
+    @test node_a.∇ ≈ node_b.∇
+    @test node_a.isinitialized == node_b.isinitialized
+    return
 end
 
 ∇ = fill(1.0, 2)
 n = 10
 tg = TreeGradient(∇, n)
-@test collect(tg) == [(1, fill(1.0, 2))]
+ans = collect(tg)
+@test length(ans) == 1
+tg_cmp(ans[1], (1, TreeGradientNode(fill(1.0, 2), true)))
 @test tg.∇ ≈ tg_sum(tg)
+@test tg.ninit == 10
 
 # make a series of insertions, and test that the state is as expected
 i, j = 2, 8
-∇i = fill(2.0, 2)
+v = 2.0
+∇i = fill(v, 2)
 insert!(tg, i, j, ∇i)
-@test collect(tg) == [(1, zeros(2)), (2, ∇i), (9, zeros(2))]
+∇i .= 0
+ans = collect(tg)
+@test length(ans) == 3
+tg_cmp(ans[1], (1, TreeGradientNode(zeros(2), false)))
+tg_cmp(ans[2], (2, TreeGradientNode(fill(v, 2), true)))
+tg_cmp(ans[3], (9, TreeGradientNode(zeros(2), false)))
 @test tg.∇ ≈ tg_sum(tg)
 @test tg.ninit == 7
 
 i, j = 2, 8
-∇i = fill(2.1, 2)
-insert!(tg, i, j, ∇i)    
-@test collect(tg) == [(1, zeros(2)), (2, ∇i), (9, zeros(2))]
+v = 2.1
+∇i = fill(v, 2)
+insert!(tg, i, j, ∇i)
+∇i .= 0
+ans = collect(tg)
+@test length(ans) == 3
+tg_cmp(ans[1], (1, TreeGradientNode(zeros(2), false)))
+tg_cmp(ans[2], (2, TreeGradientNode(fill(v, 2), true)))
+tg_cmp(ans[3], (9, TreeGradientNode(zeros(2), false)))
 @test tg.∇ ≈ tg_sum(tg)
+@test tg.ninit == 7
 
 i, j = 2, 10
-∇i = fill(3.0, 2)
+v = 3.0
+∇i = fill(v, 2)
 insert!(tg, i, j, ∇i)    
-@test collect(tg) == [(1, zeros(2)), (2, ∇i)]
+∇i .= 0
+ans = collect(tg)
+@test length(ans) == 2
+tg_cmp(ans[1], (1, TreeGradientNode(zeros(2), false)))
+tg_cmp(ans[2], (2, TreeGradientNode(fill(v, 2), true)))
 @test tg.∇ ≈ tg_sum(tg)
+@test tg.ninit == 9
 
 i, j = 4, 6
-∇i = fill(4.0, 2)
-insert!(tg, i, j, ∇i)    
-@test collect(tg) == [(1, zeros(2)), (2, zeros(2)), (4, ∇i), (7, zeros(2))]
+v = 4.0
+∇i = fill(v, 2)
+insert!(tg, i, j, ∇i)
+∇i .= 0
+ans = collect(tg)
+@test length(ans) == 4
+tg_cmp(ans[1], (1, TreeGradientNode(zeros(2), false)))
+tg_cmp(ans[2], (2, TreeGradientNode(zeros(2), false)))
+tg_cmp(ans[3], (4, TreeGradientNode(fill(v, 2), true)))
+tg_cmp(ans[4], (7, TreeGradientNode(zeros(2), false)))
 @test tg.∇ ≈ tg_sum(tg)
+@test tg.ninit == 3
 
 i, j = 1, 10
-∇i = fill(5.0, 2)
-insert!(tg, i, j, ∇i)    
-@test collect(tg) == [(1, ∇i)]
+v = 5.0
+∇i = fill(v, 2)
+insert!(tg, i, j, ∇i)
+∇i .= 0
+ans = collect(tg)
+@test length(ans) == 1
+tg_cmp(ans[1], (1, TreeGradientNode(fill(v, 2), true)))
 @test tg.∇ ≈ tg_sum(tg)
+@test tg.ninit == 10
 
 i, j = 1, 3
 ∇i = fill(1.0, 2)
-insert!(tg, i, j, ∇i)    
+insert!(tg, i, j, ∇i)
+∇i .= 0
 i, j = 4, 4
 ∇i = fill(3.0, 2)    
 insert!(tg, i, j, ∇i)
+∇i .= 0
 i, j = 5, 6
 ∇i = fill(4.0, 2)    
 insert!(tg, i, j, ∇i)
+∇i .= 0
 i, j = 2, 8
 ∇i = fill(8.0, 2)    
 insert!(tg, i, j, ∇i)
-@test collect(tg) == [(1, zeros(2)), (2, fill(8.0, 2)), (9, zeros(2))]
+∇i .= 0
+ans = collect(tg)
+@test length(ans) == 3
+tg_cmp(ans[1], (1, TreeGradientNode(zeros(2), false)))
+tg_cmp(ans[2], (2, TreeGradientNode(fill(8.0, 2), true)))
+tg_cmp(ans[3], (9, TreeGradientNode(zeros(2), false)))
 @test tg.∇ ≈ tg_sum(tg)
+@test tg.ninit == 7
+
+i, j = 1, 1
+∇j = fill(2.0, 2)
+insert!(tg, i, j, ∇j)
+∇j .= 0
+ans = collect(tg)
+@test length(ans) == 3
+tg_cmp(ans[1], (1, TreeGradientNode(fill(2.0, 2), true)))
+tg_cmp(ans[2], (2, TreeGradientNode(fill(8.0, 2), true)))
+tg_cmp(ans[3], (9, TreeGradientNode(zeros(2), false)))
+@test tg.∇ ≈ tg_sum(tg)
+@test tg.ninit == 8
+
+i, j = 2, 10
+∇i = fill(3.0, 2)
+insert!(tg, i, j, ∇i)
+∇i .= 0
+ans = collect(tg)
+@test length(ans) == 2
+tg_cmp(ans[1], (1, TreeGradientNode(fill(2.0, 2), true)))
+tg_cmp(ans[2], (2, TreeGradientNode(fill(3.0, 2), true)))
+@test tg.∇ ≈ tg_sum(tg)
+@test tg.ninit == 10
 
 # test that new memory was allocated
-∇i .= 1.0
-@test collect(tg) == [(1, zeros(2)), (2, fill(8.0, 2)), (9, zeros(2))]
+∇j .= 1.0
+tg_cmp(ans[1], (1, TreeGradientNode(fill(2.0, 2), true)))
 
 # test input validation
 i, j = 0, 3
