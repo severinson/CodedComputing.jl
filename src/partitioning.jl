c67d2469a59a3a1c19c57656e61468ccc00f18c3
Albin Severinson <albin@severinson.org>
2021-09-07 12:52:30 +0200

    Added functions for translating indices when changing the number of partitions

--- src/partitioning.jl
+++ src/partitioning.jl
@@ -1,4 +1,4 @@
-export partition
+export partition, translate_partition, align_partitions
 
 """
     partition(n::Integer, p::Integer, i::Integer)
@@ -11,4 +11,39 @@ function partition(n::Integer, p::Integer, i::Integer)
     0 < p <= n || throw(ArgumentError("p must be in [1, $n], but is $p"))    
     0 < i <= p || throw(ArgumentError("i must be in [1, $p], but is $i"))
     (div((i-1)*n, p)+1):div(i*n, p)
+end
+
+"""
+    translate_partition(n::Integer, p::Integer, q::Integer, i::Integer)
+
+Find `j` such that the `j`-th out of `q` partitions contains the first element contained in the
+`i`-th out of `p` partitions.
+"""
+function translate_partition(n::Integer, p::Integer, q::Integer, i::Integer)
+    0 < n || throw(ArgumentError("n must be positive, but is $n"))        
+    0 < p <= n || throw(ArgumentError("p must be in [1, $n], but is $p"))        
+    0 < q <= n || throw(ArgumentError("q must be in [1, $n], but is $q"))
+    0 < i <= p || throw(ArgumentError("i must be in [1, $p], but is $i"))    
+    ceil(Int, first(partition(n, p, i)) / n * q)
+end
+
+"""
+    align_partitions(n::Integer, p::Integer, q::Integer, i::Integer)
+
+Find the largest `j` smaller than or equal to `translate_partition(n, p, q, i)` such that the first
+element of the `j`-th out of `q` partitions is equal to the first element of the `i`-th out of `p`
+partitions.
+"""
+function align_partitions(n::Integer, p::Integer, q::Integer, i::Integer)
+    0 < n || throw(ArgumentError("n must be positive, but is $n"))    
+    0 < p <= n || throw(ArgumentError("p must be in [1, $n], but is $p"))        
+    0 < q <= n || throw(ArgumentError("q must be in [1, $n], but is $q"))
+    0 < i <= p || throw(ArgumentError("i must be in [1, $p], but is $i"))
+    j = translate_partition(n, p, q, i)
+    i = translate_partition(n, q, p, j)
+    while first(partition(n, q, j)) != first(partition(n, p, i))                
+        j -= 1
+        i = translate_partition(n, q, p, j)
+    end
+    j
 end
--- test/partitioning_test.jl
+++ test/partitioning_test.jl
@@ -1,7 +1,48 @@
-for n in 1:17
-    for p in 1:n
-        Is = [partition(n, p, i) for i in 1:p]
-        @test vcat(Is...) == collect(1:n)
-        @test maximum(length, Is) <= minimum(length, Is) + 1        
+@testset "partition" begin
+    for n in 1:17
+        for p in 1:n
+            Is = [partition(n, p, i) for i in 1:p]
+            @test vcat(Is...) == collect(1:n)
+            @test maximum(length, Is) <= minimum(length, Is) + 1        
+        end
     end
+end
+
+@testset "translate_partition" begin
+    n = 17
+    for p in 1:n
+        for i in 1:p
+            for q in 1:n
+                j = translate_partition(n, p, q, i)
+                @test first(partition(n, p, i)) in partition(n, q, j)
+            end
+        end
+    end
+end
+
+@testset "align_partitions" begin
+    n = 10
+    p, q, i = 4, 5, 1
+    @test align_partitions(n, p, q, i) == 1
+    p, q, i = 4, 5, 2
+    @test align_partitions(n, p, q, i) == 2
+    p, q, i = 4, 5, 3
+    @test align_partitions(n, p, q, i) == 2
+    p, q, i = 4, 5, 4
+    @test align_partitions(n, p, q, i) == 2
+    p, q, i = 4, 2, 2
+    @test align_partitions(n, p, q, i) == 1
+    p, q, i = 4, 2, 4
+    @test align_partitions(n, p, q, i) == 2
+
+    n = 17
+    for p in 1:n
+        for i in 1:p
+            for q in 1:n
+                j = align_partitions(n, p, q, i)
+                ip = translate_partition(n, q, p, j)
+                @test first(partition(n, p, ip)) == first(partition(n, q, j))
+            end
+        end
+    end    
 end
