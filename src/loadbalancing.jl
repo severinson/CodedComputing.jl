d39a95807c1a6fc98d468505c262be9326bf9e03
Albin Severinson <albin@severinson.org>
2021-09-09 11:21:33 +0200

    Added an improved load-balancer

--- src/loadbalancing.jl
+++ src/loadbalancing.jl
@@ -1,6 +1,6 @@
 # Code for load-balancing optimization
 
-using Distributions, Evolutionary
+using Random, Distributions, Evolutionary
 
 function setup_loadbalancer_channels(;chin_size=Inf, chout_size=Inf)
     chin = Channel{ProfilerOutput}(chin_size)
@@ -8,7 +8,113 @@ function setup_loadbalancer_channels(;chin_size=Inf, chout_size=Inf)
     chin, chout
 end
 
-function optimize(sim::EventDrivenSimulator, qs0; θs, comp_mcs, comp_vcs, comm_mcs, comm_vcs, min_processed_fraction::Real, populationSize::Integer=100, tournamentSize::Integer=32, mutationRate::Real=0.5, time_limit::Real=10.0, simulation_niterations::Integer=100, simulation_nsamples::Integer=10)
+function optimize2!(ps::AbstractVector, sim::EventDrivenSimulator; ∇s=zeros(length(ps)), ls=zeros(length(ps)), contribs=zeros(length(ps)), θs, comp_mcs, comp_vcs, comm_mcs, comm_vcs, min_processed_fraction::Real, time_limit::Real=1.0, simulation_niterations::Integer=100, simulation_nsamples::Integer=10)
+    0 < min_processed_fraction <= 1 || throw(ArgumentError("min_processed_fraction is $min_processed_fraction"))
+    nworkers = length(ps)
+    length(θs) == nworkers || throw(DimensionMismatch("θs has dimension $(length(θs)), but nworkers is $nworkers"))
+    length(comp_mcs) == nworkers || throw(DimensionMismatch("comp_mcs has dimension $(length(comp_mcs)), but nworkers is $nworkers"))
+    length(comp_vcs) == nworkers || throw(DimensionMismatch("comp_vcs has dimension $(length(comp_vcs)), but nworkers is $nworkers"))
+    length(comm_mcs) == nworkers || throw(DimensionMismatch("comm_mcs has dimension $(length(comm_mcs)), but nworkers is $nworkers"))
+    length(comm_vcs) == nworkers || throw(DimensionMismatch("comm_vcs has dimension $(length(comm_vcs)), but nworkers is $nworkers"))   
+
+    # setup communication latency distributions
+    for i in 1:nworkers
+        m = comm_mcs[i]
+        v = comm_vcs[i]
+        sim.comm_distributions[i] = CodedComputing.distribution_from_mean_variance(Gamma, m, v)
+    end
+
+    # helper function to run simulations
+    function simulate(ps)
+
+        # setup compute latency distributions
+        for i in 1:nworkers
+            m = comp_mcs[i] * θs[i] / ps[i]
+            v = comp_vcs[i] * θs[i] / ps[i]
+            sim.comp_distributions[i] = CodedComputing.distribution_from_mean_variance(Gamma, m, v)
+        end
+    
+        # run nsamples simulations, each consisting of niterations steps
+        ls .= 0
+        for _ in 1:simulation_nsamples
+            sim = EventDrivenSimulator(sim)
+            step!(sim, simulation_niterations)
+            ls .+= sim.nfresh ./ simulation_niterations
+        end
+        ls ./= simulation_nsamples
+    end
+
+    # estimate the gradient of the i-th element
+    function finite_diff(ps::AbstractVector, i::Integer, δ::Real=min(ps[i]-sqrt(eps(Float64)), ps[i]/10))
+        0 < δ < ps[i] || throw(ArgumentError("δ is $δ, ps[i] is $(ps[i])"))
+        0 < i <= length(ps) || throw(ArgumentError("i is $i"))
+        pi0 = ps[i]
+
+        # forward difference
+        ps[i] = pi0 + δ
+        ls = simulate(ps)
+        forward = ls[i] * θs[i] / ps[i]
+
+        # backward difference
+        ps[i] = pi0 - δ
+        ls = simulate(ps)
+        backward = ls[i] * θs[i] / ps[i]
+
+        # symmetric difference
+        ps[i] = pi0
+        (forward - backward) / 2δ
+    end
+
+    # parameters
+    μ = min_processed_fraction / nworkers
+    α = 0.1
+    
+    # run for up to time_limit seconds
+    t0 = time_ns()
+    t = t0    
+    while (t - t0) / 1e9 < time_limit && t0 <= t
+
+        # scale the workload uniformly to meet the min_processed_fraction requirement
+        contribs .= simulate(ps) .* θs ./ ps
+        s = sum(contribs)
+        ps ./= min_processed_fraction / s
+        contribs .*= min_processed_fraction / s
+
+        # estimate gradients with respect to each element
+        for i in 1:nworkers
+            ∇s[i] = finite_diff(ps, i)
+        end
+
+        # make one pass over the workers, updating the number of partitions
+        for i in 1:nworkers
+            if isapprox(contribs[i], 0) || isapprox(∇s[i], 0)
+                # the contribution or gradient is zero when the worker never participates, 
+                # so we need to increase the number of partitions                            
+                x = (1+α)*ps[i]
+            else
+                # solve for x in contribs[i] + (x - ps[i]) * ∇s[i] - μ = 0
+                # (Newton's method)
+                x = (μ - contribs[i]) / ∇s[i] + ps[i]
+                x = min(x, (1+α)*ps[i])
+                x = max(x, (1-α)*ps[i])
+                x = max(x, 1.0)
+            end
+            ps[i] = x
+        end
+        t = time_ns()
+    end
+
+    # scale the workload uniformly to meet the min_processed_fraction requirement
+    contribs .= simulate(ps) .* θs ./ ps
+    s = sum(contribs)
+    ps ./= min_processed_fraction / s
+    contribs .*= min_processed_fraction / s
+    loss = var(contribs)
+
+    ps, loss
+end
+
+function optimize(sim::EventDrivenSimulator, qs0; θs, comp_mcs, comp_vcs, comm_mcs, comm_vcs, min_processed_fraction::Real, populationSize::Integer=100, tournamentSize::Integer=32, mutationRate::Real=1.0, time_limit::Real=10.0, simulation_niterations::Integer=100, simulation_nsamples::Integer=10)
     0 < min_processed_fraction <= 1 || throw(ArgumentError("min_processed_fraction is $min_processed_fraction"))
     nworkers = length(qs0)
     length(θs) == nworkers || throw(DimensionMismatch("θs has dimension $(length(θs)), but nworkers is $nworkers"))
@@ -27,6 +133,8 @@ function optimize(sim::EventDrivenSimulator, qs0; θs, comp_mcs, comp_vcs, comm_
     # simulation helper function
     ls = zeros(nworkers)
     function simulate(qs)
+
+        # @info "evaluating $(1 ./ qs)"
 
         # setup compute latency distributions
         for i in 1:nworkers
--- test/loadbalancing_test.jl
+++ test/loadbalancing_test.jl
@@ -1,60 +1,103 @@
-chin, chout = CodedComputing.setup_loadbalancer_channels()
+using Distributions
 
-nworkers = 2
-nwait = 1
-min_processed_fraction = 0.1
-time_limit = 1.0 # must be floating-point
-θs = [0.3, 0.7]
-qs = 1 ./ [2, 3]
+@testset "smoke-test" begin
+    chin, chout = CodedComputing.setup_loadbalancer_channels()
 
-# put some random values into the load-balancer input
-Random.seed!(123)
-worker = 1
-v1 = CodedComputing.ProfilerOutput(worker, θs[worker], qs[worker], rand(), rand(), rand(), rand())
-push!(chin, v1)
+    nworkers = 2
+    nwait = 1
+    min_processed_fraction = 0.1
+    time_limit = 1.0 # must be floating-point
+    θs = [0.3, 0.7]
+    qs = 1 ./ [2, 3]
 
-worker = 2
-v2 = CodedComputing.ProfilerOutput(worker, θs[worker], qs[worker], rand(), rand(), rand(), rand())
-push!(chin, v2)
+    # put some random values into the load-balancer input
+    Random.seed!(123)
+    worker = 1
+    v1 = CodedComputing.ProfilerOutput(worker, θs[worker], qs[worker], rand(), rand(), rand(), rand())
+    push!(chin, v1)
 
-# start the load-balancer
-task = Threads.@spawn CodedComputing.load_balancer(chin, chout; min_processed_fraction, nwait, nworkers, time_limit)
+    worker = 2
+    v2 = CodedComputing.ProfilerOutput(worker, θs[worker], qs[worker], rand(), rand(), rand(), rand())
+    push!(chin, v2)
 
-# wait for up to 10 seconds for the input to be consumed
-t0 = time_ns()
-while (time_ns() - t0)/1e9 < 10 && isready(chin)
-    sleep(0.1)
-end
-if istaskfailed(task)
+    # start the load-balancer
+    task = Threads.@spawn CodedComputing.load_balancer(chin, chout; min_processed_fraction, nwait, nworkers, time_limit)
+
+    # wait for up to 10 seconds for the input to be consumed
+    t0 = time_ns()
+    while (time_ns() - t0)/1e9 < 10 && isready(chin)
+        sleep(0.1)
+    end
+    if istaskfailed(task)
+        wait(task)
+    end
+    @test !isready(chin)
+
+    # wait for up to 10 seconds for the subsystem to produce output
+    t0 = time_ns()
+    while (time_ns() - t0)/1e9 < 10 && !isready(chout)
+        sleep(0.1)
+    end
+    if istaskfailed(task)
+        wait(task)
+    end
+
+    correct1 = (1, 1)
+    correct2 = (2, 7)
+
+    @test isready(chout)
+    vout = take!(chout)
+    correct = vout.worker == 1 ? correct1 : correct2
+    # @test vout == correct
+    println(vout)
+
+    @test isready(chout)
+    vout = take!(chout)
+    correct = vout.worker == 1 ? correct1 : correct2
+    # @test vout == correct
+    println(vout)
+
+    # stop the profiler
+    close(chin)
+    close(chout)
     wait(task)
 end
-@test !isready(chin)
 
-# wait for up to 10 seconds for the subsystem to produce output
-t0 = time_ns()
-while (time_ns() - t0)/1e9 < 10 && !isready(chout)
-    sleep(0.1)
+@testset "optimizer" begin
+    nworkers = 10
+    nwait = nworkers
+    nslow = 3    
+    nsubpartitions = 160.0
+    θs = fill(1/nworkers, nworkers)
+    ps = fill(nsubpartitions, nworkers)
+
+    # per-worker compute latency
+    comp_ms = fill(1.0, nworkers)
+    comp_ms[1:nslow] .*= 2    
+    comp_vs = comp_ms ./ 100
+    comp_mcs = comp_ms ./ (θs ./ ps)
+    comp_vcs = comp_vs ./ (θs ./ ps)
+
+    # per-worker communication latency
+    comm_mcs = fill(1e-2, nworkers)
+    comm_vcs = comm_mcs ./ 100
+    
+    sim_nwait = floor(Int, nworkers/2)
+    comp_distributions = [Gamma() for _ in 1:nworkers]
+    comm_distributions = [Gamma() for _ in 1:nworkers]
+    sim = EventDrivenSimulator(;nwait=sim_nwait, nworkers, comm_distributions, comp_distributions)
+    min_processed_fraction = sim_nwait / nworkers / nsubpartitions
+
+    ps, loss = CodedComputing.optimize2!(ps, sim; θs, comp_mcs, comp_vcs, comm_mcs, comm_vcs, min_processed_fraction, time_limit=5)
+    slows = ps[1:nslow]
+    slows_mean = mean(slows)
+    fasts = ps[nslow+1:end]
+    fasts_mean = mean(fasts)
+    @test 2 / mean(slows) ≈ 1 / mean(fasts) rtol=0.1
+    for v in slows
+        @test v ≈ slows_mean rtol=0.1
+    end
+    for v in fasts
+        @test v ≈ fasts_mean rtol=0.1
+    end
 end
-if istaskfailed(task)
-    wait(task)
-end
-
-correct1 = (1, 1)
-correct2 = (2, 7)
-
-@test isready(chout)
-vout = take!(chout)
-correct = vout.worker == 1 ? correct1 : correct2
-# @test vout == correct
-println(vout)
-
-@test isready(chout)
-vout = take!(chout)
-correct = vout.worker == 1 ? correct1 : correct2
-# @test vout == correct
-println(vout)
-
-# stop the profiler
-close(chin)
-close(chout)
-wait(task)
