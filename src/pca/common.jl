6a90b3d5b1579a6f3edf55465286ed05fb824f63
Albin Severinson <albin@severinson.org>
2021-09-21 14:00:00 +0200

    Updated the default for lbminimprovement

--- src/pca/common.jl
+++ src/pca/common.jl
@@ -88,7 +88,7 @@ function parse_commandline(isroot::Bool)
             range_tester = (x) -> 0 < x
         "--lbminimprovement"
             help = "Minimum improvement required for the load-balancer to accept a new solution"
-            default = 10.0
+            default = 0.9
             arg_type = Float64
             range_tester = (x) -> 1 <= x
         "--lbaggressive"
